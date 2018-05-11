d0c0662d9ea92094d4d99be0c95fe4d1c5db4dda
lhenrique <luiz.henrique@watchguard.com>
2018-05-11 16:34:57 -0300

    Renaming constants.

--- src/main/java/br/pegz/tutorials/rightcourt/score/ScoreNotifierService.java
+++ src/main/java/br/pegz/tutorials/rightcourt/score/ScoreNotifierService.java
@@ -6,9 +6,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
 
-import java.io.UnsupportedEncodingException;
-import java.net.ConnectException;
-
 @Slf4j
 @Service
 public class ScoreNotifierService {
@@ -22,7 +19,7 @@ public class ScoreNotifierService {
     public void notifyFoePoint(Integer count) {
         try {
             log.info("Notifying point for LEFT in the play #{}", count);
-            rabbitTemplate.convertAndSend(AMQPConfig.topicExchangeName, "scores-java", getNotifyScore(Side.LEFT, count));
+            rabbitTemplate.convertAndSend(AMQPConfig.SCORE_QUEUE_EXCHANGE, "scores-java", getNotifyScore(Side.LEFT, count));
         } catch (Exception ex) {
             log.error("Rabbit is out of reach, point is nilled");
             log.info("Point is nilled, PRACTICE MODE!!!, but was {}", getNotifyScore(Side.LEFT, count));
@@ -32,7 +29,7 @@ public class ScoreNotifierService {
     public void notifyMyPoint(Integer count) {
         try {
             log.info("Notifying point for RIGHT in the play #{}", count);
-            rabbitTemplate.convertAndSend(AMQPConfig.topicExchangeName, "scores-java", getNotifyScore(Side.RIGHT, count));
+            rabbitTemplate.convertAndSend(AMQPConfig.SCORE_QUEUE_EXCHANGE, "scores-java", getNotifyScore(Side.RIGHT, count));
         } catch (Exception ex) {
             log.error("Rabbit is out of reach, point is nilled", ex);
             log.info("Point is nilled, PRACTICE MODE!!!, but was {}", getNotifyScore(Side.RIGHT, count));
