33c58af002686e3223343b30c9cba6bf1c270921
lhenrique <luiz.henrique@watchguard.com>
2018-05-15 10:39:33 -0300

    Implemented async playing methods in playService.

--- src/main/java/br/pegz/tutorials/rightcourt/serve/service/PlayService.java
+++ src/main/java/br/pegz/tutorials/rightcourt/serve/service/PlayService.java
@@ -1,5 +1,6 @@
 package br.pegz.tutorials.rightcourt.serve.service;
 
+import br.pegz.tutorials.rightcourt.configuration.PlayExchange;
 import br.pegz.tutorials.rightcourt.persistence.Play;
 import br.pegz.tutorials.rightcourt.persistence.enums.Height;
 import br.pegz.tutorials.rightcourt.persistence.enums.Side;
@@ -7,13 +8,12 @@ import br.pegz.tutorials.rightcourt.persistence.enums.Speed;
 import br.pegz.tutorials.rightcourt.score.ScoreNotifierService;
 import br.pegz.tutorials.rightcourt.serve.exception.PointException;
 import br.pegz.tutorials.rightcourt.serve.resource.CourtResource;
-import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Processor;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 
 import java.security.SecureRandom;
-import java.util.List;
 import java.util.Random;
 
 @Slf4j
@@ -21,7 +21,6 @@ import java.util.Random;
 public final class PlayService {
 
     private final CourtResource courtResource;
-    private final Random random = new SecureRandom();
     private final ScoreNotifierService scoreNotifierService;
 
     public PlayService(CourtResource courtResource, ScoreNotifierService scoreNotifierService) {
@@ -29,29 +28,30 @@ public final class PlayService {
         this.scoreNotifierService = scoreNotifierService;
     }
 
-    public Flux<Play> serve() throws PointException {
-        List<Play> plays = Lists.newArrayList(getServePlay());
-        Play incomingPlay = courtResource.sendAndRetrieve(getServePlay());
-        while (canContinuePlay(incomingPlay)) {
-            log.info("Received play: {}", incomingPlay);
-            plays.add(incomingPlay);
-            Play myPlay = handlePlay(incomingPlay);
-            plays.add(myPlay);
-            incomingPlay = courtResource.sendAndRetrieve(myPlay);
+    public void serve() {
+        courtResource.sendPlay(getServePlay());
+    }
+
+    @StreamListener(PlayExchange.PLAY_INPUT)
+    public void handlePlay(Play incomingPlay) throws PointException {
+        if(canContinuePlay(incomingPlay)) {
+            courtResource.sendPlay(buildResponsePlay(incomingPlay));
+        } else {
+            notifyPoint(incomingPlay);
         }
-        notifyPoint(incomingPlay);
-        return Flux.fromStream(plays.stream());
     }
 
     private boolean canContinuePlay(Play incomingPlay) {
         return !isLeftPoint(incomingPlay) && !isRightPoint(incomingPlay);
     }
 
-    private void notifyPoint(Play incomingPlay) {
+    private void notifyPoint(Play incomingPlay) throws PointException {
         if (isLeftPoint(incomingPlay)) {
             scoreNotifierService.notifyFoePoint(incomingPlay.getCount());
+            throw new PointException(Side.LEFT);
         } else if (isRightPoint(incomingPlay)) {
             scoreNotifierService.notifyMyPoint(incomingPlay.getCount());
+            throw new PointException(Side.RIGHT);
         }
     }
 
@@ -63,32 +63,15 @@ public final class PlayService {
         return Side.NET == incomingPlay.getInnerSide() || Side.OUTSIDE == incomingPlay.getInnerSide();
     }
 
-    private Play getRespondSpeed(Play incomingPlay) {
-        Speed responseSpeed = Speed.random();
-        Height responseHeight = Height.random();
-        Side responseInnerSide = Side.random();
+    private Play buildResponsePlay(Play incomingPlay) {
         return Play.builder()
                 .count(incomingPlay.getCount()+1)
                 .effect(!incomingPlay.getEffect())
-                .speed(responseSpeed)
-                .height(responseHeight)
+                .speed(Speed.random())
+                .height(Height.random())
                 .incomingSide(Side.RIGHT)
-                .innerSide(responseInnerSide)
+                .innerSide(Side.random())
                 .build();
-    }
-
-
-    public Play handlePlay(Play incomingPlay) throws PointException {
-        if(!isRightPoint(incomingPlay) && !isLeftPoint(incomingPlay)) {
-            return getRespondSpeed(incomingPlay);
-        } else if (isLeftPoint(incomingPlay)) {
-            scoreNotifierService.notifyFoePoint(incomingPlay.getCount());
-            throw new PointException(Side.LEFT);
-        } else if (isRightPoint(incomingPlay)) {
-            scoreNotifierService.notifyMyPoint(incomingPlay.getCount());
-            throw new PointException(Side.RIGHT);
-        }
-        return null;
     }
 
     private Play getServePlay() {
