cf6acd774dc2fc13ae5f054541a9567cb7cd107e
lhenrique <luiz.henrique@watchguard.com>
2018-05-15 10:56:08 -0300

    Created exchanges interface.

---
+++ src/main/java/br/pegz/tutorials/rightcourt/configuration/PlayExchange.java
@@ -0,0 +1,9 @@
+package br.pegz.tutorials.rightcourt.configuration;
+
+public interface PlayExchange {
+
+    String PLAY_INPUT = "playInput";
+    String PLAY_OUTPUT = "playOutput";
+    String SCORE_OUTPUT = "scoreOutput";
+
+}
