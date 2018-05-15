c42039093704c23eb649d5e743415ad48750fd54
lhenrique <luiz.henrique@watchguard.com>
2018-05-15 10:30:56 -0300

    Removed Rest Template.

--- src/main/java/br/pegz/tutorials/rightcourt/RightCourtApplication.java
+++ src/main/java/br/pegz/tutorials/rightcourt/RightCourtApplication.java
@@ -2,9 +2,6 @@ package br.pegz.tutorials.rightcourt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class RightCourtApplication {
@@ -13,8 +10,4 @@ public class RightCourtApplication {
 		SpringApplication.run(RightCourtApplication.class, args);
 	}
 
-	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder) {
-		return builder.build();
-	}
 }
